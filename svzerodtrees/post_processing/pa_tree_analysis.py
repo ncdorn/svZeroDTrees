52e044dbc604a0c743d88b4c2db098fe99103c2c
Nick Dorn <ndorn@stanford.edu>
2023-09-19 10:51:52 -0700

    updated tree analysis

--- svzerodtrees/post_processing/pa_tree_analysis.py
+++ svzerodtrees/post_processing/pa_tree_analysis.py
@@ -3,7 +3,7 @@ from svzerodtrees.utils import *
 
 
 
-def plot_distal_wss(config, result):
+def plot_distal_wss(config, result, fig_dir):
     '''
     plot the wss at the outlets of the model
 
@@ -13,7 +13,10 @@ def plot_distal_wss(config, result):
 
     plt.hist(get_outlet_data(config, result, "wss"), bins=20)
 
-    plt.show()
+    plt.title('Histogram of PA distal vessel WSS')
+
+    plt.tight_layout()
+    plt.savefig(str(fig_dir + '/') + 'distal_wss' + '.png')
 
 
 def plot_micro_wss(trees, max_d=.015):
@@ -38,7 +41,7 @@ def plot_micro_wss(trees, max_d=.015):
         get_distal_wss(tree.root)
     
     plt.hist(wss_list, bins=100)
-    plt.title('Histogram of distal WSS')
+    plt.title('Histogram of wss for vessels < ' + str(max_d * 1000) + ' um')
 
 
 def plot_outlet_flow_histogram():
