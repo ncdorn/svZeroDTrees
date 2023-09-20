7249544ab3c1266ff022dda848a1976a63515748
Nicholas Cannon Dorn <ndorn@sh02-01n57.int>
2023-09-20 12:14:18 -0700

    added config w trees save

--- svzerodtrees/preop.py
+++ svzerodtrees/preop.py
@@ -389,6 +389,9 @@ def construct_cwss_trees(config: dict, result, log_file=None, d_min=0.0049, vis_
 
     # if vis_trees:
     #     visualize_trees(config, roots, fig_dir=fig_dir, fig_name='_preop')
+    
+    with open('fig_dir/config_w_trees.json', 'w') as ff:
+        json.dump(config, ff)
 
     return trees
 
