bb785ef59d42a2b13a0aace2459e5c8ad49620b3
ncdorn <ndorn@stanford.edu>
2023-09-28 14:46:08 -0700

    sync

--- models/AS1_SU0308_prestent/run_experiment.py
+++
@@ -1,9 +0,0 @@
-from svzerodtrees.interface import *
-import os
-
-'''
-example script for running an svZeroDTrees experiment from a json config file
-'''
-if __name__ == '__main__':
-    os.chdir('experiments')
-    run_from_file('AS1_cwss_adaptation.json', vis_trees=True)
