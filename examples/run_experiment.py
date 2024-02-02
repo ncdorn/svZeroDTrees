e64a4764584230b00f379dd8e788286beb93dcd6
ndorn <ndorn@stanford.edu>
2024-02-01 17:27:37 -0800

    sync

--- examples/run_experiment.py
+++
@@ -1,9 +0,0 @@
-from svzerodtrees.interface import *
-import os
-
-'''
-example script for running an svZeroDTrees experiment from a json config file
-'''
-if __name__ == '__main__':
-    os.chdir('models/LPA_RPA_0d_steady/experiments')
-    run_from_file('LPA_RPA_ps_adapt_9.8.23.json', vis_trees=True)
