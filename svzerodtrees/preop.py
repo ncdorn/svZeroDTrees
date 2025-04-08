Update weights in optimize_impedance_bcs for better optimization; fix rpa_split calculation in SimulationDirectory

svzerodtrees/preop.py
@@ -774,7 +774,7 @@ def optimize_impedance_bcs(config_handler, mesh_surfaces_path, clinical_targets,
                     # diastolic pressure < wedge pressure so we neglect it in the optimization
                     weights = np.array([1, 0, 1])
                 else:
-                    weights = np.array([1.1, 1.1, 1])
+                    weights = np.array([1.2, 1, 1.1])
                 
                 pressure_loss = np.sum(np.dot(np.abs(np.array(pa_config.P_mpa) - np.array(clinical_targets.mpa_p)) / clinical_targets.mpa_p, weights)) ** 2 * 100
 

svzerodtrees/simulation_directory.py
@@ -694,7 +694,7 @@ class SimulationDirectory:
 
         # get rpa split
         lpa_flow, rpa_flow = self.flow_split()
-        rpa_split = sum(lpa_flow['mean'].values()) / (sum(lpa_flow['mean'].values()) + sum(rpa_flow['mean'].values()))
+        rpa_split = sum(rpa_flow['mean'].values()) / (sum(lpa_flow['mean'].values()) + sum(rpa_flow['mean'].values()))
 
         targets = {'mean': np.mean(pressure) / 1333.2, 'sys': np.max(pressure) / 1333.2, 'dia': np.min(pressure) / 1333.2, 'rpa_split': rpa_split}
 
@@ -731,8 +731,8 @@ class SimulationDirectory:
             # compute loss
             loss = (abs(mean_pressure - targets['mean']) ** 2 +
                     abs(sys_pressure - targets['sys']) ** 2 +
-                    abs(dia_pressure - targets['dia']) ** 2)
-                    # abs(rpa_split - targets['rpa_split']) * 100 ** 2)
+                    abs(dia_pressure - targets['dia']) ** 2 +
+                    abs(rpa_split - targets['rpa_split']) * 100 ** 2)
             print(f"pressures: {int(sys_pressure * 100) / 100} / {int(dia_pressure * 100) / 100}/{int(mean_pressure * 100) / 100} mmHg, target: {int(targets['sys'] * 100) / 100}/{int(targets['dia'] * 100) / 100}/{int(targets['mean'] * 100) / 100} mmHg")
             print(f"RPA split: {rpa_split}, target: {targets['rpa_split']}")
             print(f"Current nonlinear resistances: LPA = {nonlinear_resistance[0]}, RPA = {nonlinear_resistance[1]}, Loss = {loss}")