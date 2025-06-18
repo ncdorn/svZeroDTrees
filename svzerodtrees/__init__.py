a5d389b7fe1aad981b379fb7c00868ac3e528d42
ncdorn <ndorn@stanford.edu>
2025-06-18 16:38:52 -0700

    adjust top level init

--- svzerodtrees/__init__.py
+++ svzerodtrees/__init__.py
@@ -2,3 +2,6 @@
 this is the top-level package for svzerodtrees
 '''
 
+from .microvasculature import StructuredTree
+from .io import ConfigHandler, Inflow
+from .simulation import Simulation, SimulationDirectory
