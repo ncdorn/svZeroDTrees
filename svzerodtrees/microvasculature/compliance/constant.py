315916c2e78f82a495f3e29f38d7a7aa166c637a
ncdorn <ndorn@stanford.edu>
2025-08-06 14:30:24 -0700

    fix: streamline parameter initialization in ConstantCompliance and OlufsenCompliance classes feat: add d_min, alpha, and beta to TreeParameters summary output

--- svzerodtrees/microvasculature/compliance/constant.py
+++ svzerodtrees/microvasculature/compliance/constant.py
@@ -3,7 +3,7 @@ from .base import ComplianceModel
 class ConstantCompliance(ComplianceModel):
     def __init__(self, value: float):
         self.value = value
-        self.params['Eh/r'] = value
+        self.params = {"Eh/r": value}
 
     def evaluate(self, r: float) -> float:
         return self.value
